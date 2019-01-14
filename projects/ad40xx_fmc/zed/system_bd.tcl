9d66d44f189dad0707188d236badc58939d56b94
Istvan Csomortani <istvan.csomortani@analog.com>
2019-01-14 08:28:32 +0000

    ad40xx: Add additional comments for rate definition

--- projects/ad40xx_fmc/zed/system_bd.tcl
+++ projects/ad40xx_fmc/zed/system_bd.tcl
@@ -7,7 +7,10 @@ set spi_clk_ref_frequency 100
 # specify ADC resolution -- supported resolutions 16/18/20 bits
 set adc_resolution 20
 
-# specify ADC sampling rate in samples/seconds -- default is 1.8 MSPS
+# specify ADC sampling rate in samples/seconds
+
+# NOTE: This rate can be set just in turbo mode -- if turbo mode is not used
+# the max rate should be 1.6 MSPS
 set adc_sampling_rate 1800000
 
 source ../common/ad40xx_bd.tcl
